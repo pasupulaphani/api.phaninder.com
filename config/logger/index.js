8e025ee049c2db3e94169e5156f25656b2e175c6
pasupulaphani <pasupulaphani@gmail.com>
2014-02-15 04:36:56 +0000

    logging setup fro secondary

--- config/logger/index.js
+++ config/logger/index.js
@@ -22,6 +22,9 @@ switch(process.env.NODE_ENV) {
   case 'prod':
   	Logger = logEntries();
     break;
+  case 'second':
+    Logger = require('./bunyan');
+    break;
   case 'stage':
     Logger = logEntries();
     break;
