b96ad44285fdef30e4be4ac77130b9cf3583a813
pasupulaphani <pasupulaphani@gmail.com>
2014-10-29 00:56:06 +0000

    removing signup route

--- config/routes/login.js
+++ config/routes/login.js
@@ -3,8 +3,8 @@ var users = require('../../app/controllers/users');
 module.exports = function (app) {
 
 	// Shouldn't require this unless you screwed database
-	app.get('/signup', users.createShow);
-	app.post('/signup', users.create);
+	// app.get('/signup', users.createShow);
+	// app.post('/signup', users.create);
 
 	app.get('/login', users.loginShow);
 	app.post('/login', users.login);
