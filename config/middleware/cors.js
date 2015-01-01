9f2c755fb6cf1a13f3750b43bce743f9dfc297c5
pasupulaphani <pasupulaphani@gmail.com>
2015-01-01 15:54:34 +0000

    aded some conf

--- config/middleware/cors.js
+++ config/middleware/cors.js
@@ -1,7 +1,11 @@
 // ## CORS middleware
 //
+
+var config = require('../config');
+
 var allowCrossDomain = exports.allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+
+    res.header('Access-Control-Allow-Origin', config.staticHost);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-XSRF-TOKEN');
     res.header('Access-Control-Allow-Credentials', true);
